b4b673d2eb1280f1215b6b2c1c2fb4a5d09f8cd4
Paul <91368295+PaulKeptene@users.noreply.github.com>
2021-10-07 20:49:22 +0300

    Fix T6 v2

--- src/com/company/T6.java
+++ src/com/company/T6.java
@@ -16,13 +16,13 @@ public class T6 {
         }
         System.out.print("Четные числа:");
         for (int i = 0; i < n; i++) {
-            if ((m[i] % 2 == 0)&(m[i]!=0)) {
+            if ((m[i] % 2 == 0) & (m[i] != 0)) {
                 System.out.print(m[i] + " ");
             }
         }
         System.out.print("\nНе четные числа:");
         for (int i = 0; i < n; i++) {
-            if ((m[i] % 2 != 0)&(m[i]!=0)) {
+            if ((m[i] % 2 != 0) & (m[i] != 0)) {
                 System.out.print(m[i] + " ");
             }
         }
@@ -38,19 +38,19 @@ public class T6 {
         System.out.println("\nМинимальное число:" + min + "\nМаксимальное число:" + max);
         System.out.print("Числа, которые делятся на 3 или на 9:");
         for (int i = 0; i < n; i++) {
-            if ((m[i] % 3 == 0)&(m[i]!=0) | (m[i] % 9 == 0)&(m[i]!=0)) {
+            if ((m[i] % 3 == 0) & (m[i] != 0) | (m[i] % 9 == 0) & (m[i] != 0)) {
                 System.out.print(m[i] + " ");
             }
         }
         System.out.print("\nЧисла, которые делятся на 5 или на 7:");
         for (int i = 0; i < n; i++) {
-            if ((m[i] % 5 == 0)&(m[i]!=0) | (m[i] % 7 == 0)&(m[i]!=0)) {
+            if ((m[i] % 5 == 0) & (m[i] != 0) | (m[i] % 7 == 0) & (m[i] != 0)) {
                 System.out.print(m[i] + " ");
             }
         }
         System.out.print("\nВсе трехзначные числа, в десятичной записи которых нет одинаковых цифр:");
         for (int i = 0; i < n; i++) {
-            if ((m[i] < 1000)&(m[i]!=0)&(m[i] > -1000)) {
+            if ((m[i] < 1000) & (m[i] != 0) & (m[i] > -1000)) {
                 int x = m[i] / 100;
                 int y = ((m[i] % 100) / 10);
                 int z = (m[i] % 10);
@@ -61,23 +61,45 @@ public class T6 {
         }
         System.out.print("\n«Счастливые» числа:");
         for (int i = 0; i < n; i++) {
-            String s = Integer.toString(m[i]*-1);
-            String[] s1 = s.split("");
-            if ((s.length() % 2 == 0)&(m[i]!=0)) {
-                int med = s.length() / 2;
-                int x, y = 0, c, v = 0;
-                for (int j = 0; j < med; j++) {
-                    x = Integer.parseInt(s1[j]);
-                    y = y + x;
+            if (m[i] < 0) {
+                String s = Integer.toString(m[i] * -1);
+                String[] s1 = s.split("");
+                if ((s.length() % 2 == 0) & (m[i] != 0)) {
+                    int med = s.length() / 2;
+                    int x, y = 0, c, v = 0;
+                    for (int j = 0; j < med; j++) {
+                        x = Integer.parseInt(s1[j]);
+                        y = y + x;
+                    }
+                    for (int k = med; k < s.length(); k++) {
+                        c = Integer.parseInt(s1[k]);
+                        v = v + c;
+                    }
+                    int p1 = y;
+                    int p2 = v;
+                    if (p1 == p2) {
+                        System.out.print(m[i] + " ");
+                    }
                 }
-                for (int k = med; k < s.length(); k++) {
-                    c = Integer.parseInt(s1[k]);
-                    v = v + c;
-                }
-                int p1 = y;
-                int p2 = v;
-                if (p1 == p2) {
-                    System.out.print(m[i] + " ");
+            } else {
+                String s = Integer.toString(m[i]);
+                String[] s1 = s.split("");
+                if ((s.length() % 2 == 0) & (m[i] != 0)) {
+                    int med = s.length() / 2;
+                    int x, y = 0, c, v = 0;
+                    for (int j = 0; j < med; j++) {
+                        x = Integer.parseInt(s1[j]);
+                        y = y + x;
+                    }
+                    for (int k = med; k < s.length(); k++) {
+                        c = Integer.parseInt(s1[k]);
+                        v = v + c;
+                    }
+                    int p1 = y;
+                    int p2 = v;
+                    if (p1 == p2) {
+                        System.out.print(m[i] + " ");
+                    }
                 }
             }
         }
